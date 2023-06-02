fc779bc2f117e1038f88a045ab25f09a4c30fa85
PaulaScharf <paulascharf@gmx.de>
2023-06-02 16:32:23 +0200

    fix: add source and remove resultpercentage if no result given for survey

--- components/Tiles/Survey/Answer.tsx
+++ components/Tiles/Survey/Answer.tsx
@@ -4,6 +4,7 @@ import { ComponentPropsWithRef } from 'react'
 import { animated, AnimatedProps } from '@react-spring/web'
 import AnimatedNumber from '@/components/Elements/Animated/AnimatedNumber'
 import Title from '@/components/Elements/Title'
+import { Spacer } from '@/components/Elements/Spacer'
 
 export type SurveyAnswerProps = AnimatedProps<ComponentPropsWithRef<'div'>> & {
   percent: number
@@ -17,18 +18,40 @@ export default function SurveyAnswer({
 }: SurveyAnswerProps) {
   return (
     <animated.div className="absolute left-0 top-0 w-full" style={{ ...props }}>
-      <div className="flex w-full flex-col gap-4 md:flex-row">
-        <Title as="h1">
-          {percent != null ? 
-          <><AnimatedNumber decimals={1}>{percent}</AnimatedNumber></> 
-          : '??'}
-          <span className="font-normal">%</span>
-        </Title>
-        <Title as="h4">
-          {percent != null ?
-          text : 
-           'Die Bürger*innen-Umfrage wird im Sommer 2023 erhoben und ausgewertet. Sobald die Ergebnisse vorliegen, findet sich an dieser Stelle die Antwort. '}</Title>
-      </div>
+      {percent != null ? (
+        <>
+          <div className="space-y-1">
+            <div className="flex w-full flex-col gap-4 md:flex-row">
+              <Title as="h1">
+                <AnimatedNumber decimals={1}>{percent}</AnimatedNumber>
+                <span className="font-normal">%</span>
+              </Title>
+              <Title as="h4">{text}</Title>
+            </div>
+            <div className="flex flex-1 space-x-2 text-xs">
+              <Title as="h7" font="semibold">
+                Datenstand: 2022
+              </Title>
+              <Title as="h7" font="normal">
+                Quelle:{' '}
+                <a
+                  className="underline"
+                  href="https://www.stadt-muenster.de/stadtentwicklung"
+                >
+                  https://www.stadt-muenster.de/stadtentwicklung
+                </a>
+              </Title>
+            </div>
+            <Spacer />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex w-full flex-col gap-4 md:flex-row">
+            <Title as="h4">{text}</Title>
+          </div>
+        </>
+      )}
     </animated.div>
   )
 }
