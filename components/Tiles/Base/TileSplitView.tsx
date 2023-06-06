1088e834dc7f1b37c8a020528b45e97de6e40f4d
PaulaScharf <paulascharf@gmx.de>
2023-06-06 11:21:04 +0200

    feat: have tile split view do rows on a bigger screen

--- components/Tiles/Base/TileSplitView.tsx
+++ components/Tiles/Base/TileSplitView.tsx
@@ -2,7 +2,7 @@ import { cx } from 'class-variance-authority'
 import { HTMLAttributes } from 'react'
 
 export function TileSplitView(props: HTMLAttributes<HTMLDivElement>) {
-  return <div className="flex flex-col gap-10 lg:flex-row" {...props} />
+  return <div className="flex flex-col gap-10 2xl:flex-row" {...props} />
 }
 
 TileSplitView.Left = function LeftView(props: HTMLAttributes<HTMLDivElement>) {
